d8aac3a22faea464f84da05b33e1b667a7b88696
Eric Saxby <sax@livinginthepast.org>
2014-03-18 14:15:01 -0700

    Initial commit

---
+++ proxy.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	port := flag.String("port", "8080", "Specifies which port to proxy in front of")
+	cert := flag.String("cert", "cert.pem", "Absolute path to cert.pem")
+	key := flag.String("key", "key.pem", "Absolute path to key.pem")
+
+	flag.Parse()
+
+	log.Println("Proxying port: " + *port)
+	log.Println("Loading cert at: " + *cert)
+	log.Println("Loading key at: " + *key)
+
+	remote, err := url.Parse("http://localhost:" + *port)
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	http.HandleFunc("/", handler(proxy))
+
+	go func() {
+		err = http.ListenAndServe(":80", nil)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	err = http.ListenAndServeTLS(":443", *cert, *key, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var protocol string
+		if r.TLS != nil {
+			protocol = "https"
+		} else {
+			protocol = "http"
+		}
+		log.Println(r.Method, protocol, r.URL)
+		r.Header.Add("X-Forwarded-Proto", protocol)
+		r.Header.Add("Host", r.Host)
+		p.ServeHTTP(w, r)
+	}
+}
