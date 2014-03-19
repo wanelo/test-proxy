93aae9f4b278b69151db05e4fd276ae64b0490b8
Eric Saxby <sax@livinginthepast.org>
2014-03-19 14:26:27 -0700

    Only log successful requests on --verbose

--- proxy.go
+++ proxy.go
@@ -12,6 +12,7 @@ func main() {
 	port := flag.String("port", "8080", "Specifies which port to proxy in front of")
 	cert := flag.String("cert", "cert.pem", "Absolute path to cert.pem")
 	key := flag.String("key", "key.pem", "Absolute path to key.pem")
+	verbose := flag.Bool("verbose", false, "Log requests to stdout")
 
 	flag.Parse()
 
@@ -25,7 +26,7 @@ func main() {
 	}
 
 	proxy := httputil.NewSingleHostReverseProxy(remote)
-	http.HandleFunc("/", handler(proxy))
+	http.HandleFunc("/", handler(proxy, verbose))
 
 	go func() {
 		err = http.ListenAndServe(":80", nil)
@@ -40,7 +41,7 @@ func main() {
 	}
 }
 
-func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+func handler(p *httputil.ReverseProxy, verbose *bool) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var protocol string
 		if r.TLS != nil {
@@ -48,7 +49,11 @@ func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) 
 		} else {
 			protocol = "http"
 		}
-		log.Println(r.Method, protocol, r.URL)
+
+    if *verbose {
+      log.Println(r.Method, protocol, r.URL)
+    }
+
 		r.Header.Add("X-Forwarded-Proto", protocol)
 		r.Header.Add("Host", r.Host)
 		p.ServeHTTP(w, r)
